5c42ec6fdfeb3037a0a7dae7445d9af5b9423fc2
khammett325995 <khammett325995@gmail.com>
2025-07-11 12:42:06 -0700

    first commit

---
+++ hearing-accessibility.py
@@ -0,0 +1,35 @@
+# This file would store the code for each hearing accessibility option.
+
+# Digital Prototype Instructions:
+# For the simulation only, click the space bar to return back to the game.
+
+# Project Documentation:
+# While this is being shown as a public repository due to simulation purposes, the intent is that using the research to show the needs that
+# could be created as it's own project with further development as an extension or separate piece of software to give devlopers a foundation 
+# into providing accesibility options especially for those of visual and hearing needs into any program at the start of developement
+# instead of having to take large amounts of development time to develop something that might already be made somewhere else. That way, the 
+# development can focus on creating more features and to hopefully improve development time speeds and to add more customizability and help.
+
+# Once the file is on a computer screen, it would be able to be saved in your own project and could be edited at any time.
+# For developers, the intent is that there would be an option to make it to where players can edit the software or not especially with the 
+# concepts of modding but also with the thoughts of creating more solutions in a community-base to help in growing accessibility in programs but 
+# especially with video games as a focus.
+# For the players, it would help for also creating new features and improving further on accessibility to help people with assistance 
+# beyond what has been provided.
+
+# Any imports such as for AI models, additional assets for adding vision accessibility would go here.
+# Examples would be an AI model that helps for adding closed captioning to what appears when running the game.
+# User Interface imports would also be supported. For the simulation, it all is a placeholder as a single file.
+
+# Listed below would be a function for each of the options that are used at the start of the software.
+# The options that would be available no matter what are shown in the digital prototype based on previously completed user research would 
+# be available initially in this file.
+
+# Closed Captioning Code Placeholder
+
+# Automizing Visual Cues Code Placeholder
+
+# Haptic Feedback Code Placeholder
+
+# User Interface Hearing Accessibility Menu Code Placeholder
+
---
+++ vision-accessibility.py
@@ -0,0 +1,37 @@
+# This file would store the code for each vision accessibility option.
+
+# Digital Prototype Instructions: For the simulation only, click the space bar to return back to the game.
+ 
+# Project Documentation:
+# While this is being shown as a public repository due to simulation purposes, the intent is that using the research to show the needs that
+# could be created as it's own project with further development as an extension or separate piece of software to give devlopers a foundation 
+# into providing accesibility options especially for those of visual and hearing needs into any program at the start of developement
+# instead of having to take large amounts of development time to develop something that might already be made somewhere else. That way, the 
+# development can focus on creating more features and to hopefully improve development time speeds and to add more customizability and help.
+
+# Once the file is on a computer screen, it would be able to be saved in your own project and could be edited at any time.
+# For developers, the intent is that there would be an option to make it to where players can edit the software or not especially with the 
+# concepts of modding but also with the thoughts of creating more solutions in a community-base to help in growing accessibility in programs but 
+# especially with video games as a focus.
+# For the players, it would help for also creating new features and improving further on accessibility to help people with assistance 
+# beyond what has been provided.
+
+# Any imports such as for AI models, additional assets for adding vision accessibility, and additional software imports would go here.
+# Examples would be the ADA supported fonts as imports. 
+# User Interface imports would also be supported. For the simulation, it all is a placeholder as a single file.
+
+# Listed below would be a function for each of the options that are used at the start of the software.
+# The options that would be available no matter what are shown in the digital prototype based on previously completed user research would 
+# be available initially in this file.
+
+# A function for chosing which features would initially run. Initially, this would include the listed functions below but can be 
+# changed to add or remove features by updating the file.
+
+# Color Blindness Modes Code Placeholder (This would start with a few types that are seen in many games)
+
+# Font Customization Code Placeholder (The code would also allow for font imports of any type with a base formating in ADA supported fonts)
+
+# Motion Blur Code Placeholder (This code would be a smaller base due to how mixed reactions were present but a need still showed.)
+
+# User Interface Vision Accessibility Menu Code Placeholder
+
